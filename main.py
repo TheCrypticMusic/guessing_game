61927eb81a97b9a7f6dc80eb6f47541aae591846
Daniel Gibson <gibsonlp@live.co.uk>
2020-08-06 21:41:47 +0100

    Deleted GUI.py

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-import random as r 
+from tkinter import *
+import random
 
-def random_number():
-    return r.randint(1, 20)
-
-# def user_choice():
-#     while True:
-#         try:    
-#             user_input = int(input('Please enter a number (1 - 20): '))
-#             if user_input == random_number():
-#                 print('Correct')
-#                 break
-#             elif user_input < random_number():
-#                 print('Number is higher')
-#             else:    
-#                 print('Number is lower')
-#         except ValueError:
-#             print("ENTER A NUMBER")
+num = random.randint(1, 20)
 
 
 
-random_number()
-#user_choice()
+def user_guess(event=None):
+    choice = guess.get()
+    if int(choice) == num:
+        print('True')
+    else:
+        print('False')
+
+
+#def higher_or_lower():
+    #if user_guess():
+        #return 'Number is lower'
+    #else:
+        #return 'Number is higher'
+
+root = Tk()
+root.title('Guessing Game')
+root.geometry('400x200')    
+
+Label(root, text='RULES OF THE GAME\n\n\
+    You have 5 turns to guess the number between 1 - 20.',).pack(side=TOP, pady=5)
+
+Label(root, text=f'Test').pack(pady=10)
+
+guess = Entry(root)
+guess.bind('<Return>', user_guess)
+guess.pack(side=TOP, pady=5)
+
+
+root.mainloop()
+
