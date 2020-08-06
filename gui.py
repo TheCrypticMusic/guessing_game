522413e062cf7146c740cfb4fd4d5914fa7206e9
TheCrypticMusic <44878496+TheCrypticMusic@users.noreply.github.com>
2020-08-06 16:03:31 +0100

    GUI.py update
    Added the ability for the user to input their number

--- gui.py
+++ gui.py
@@ -2,18 +2,28 @@ from tkinter import *
 from main import random_number
 
 
+num = random_number()  
+
+def user_guess(event=None):
+    if int(guess.get()) == num:
+        print('Correct')
+    else:
+        print('Incorrect')
+
+
 root = Tk()
 root.title('Guessing Game')
-root.geometry('400x200')
+root.geometry('400x200')    
 
 Label(root, text='RULES OF THE GAME\n\n\
     You have 5 turns to guess the number between 1 - 20.',).pack(side=TOP)
 
-Label(root, text='ENTER A NUMBER').pack(pady=10)
+Label(root, text=f'{num}').pack(pady=10)
 
-entry = Entry(root)
-entry.bind("<Return>")
-entry.pack(side=TOP, pady=5)
+guess = Entry(root)
+guess.bind('<Return>', user_guess)
+guess.pack(side=TOP, pady=5)
+
 
 
 root.mainloop()
